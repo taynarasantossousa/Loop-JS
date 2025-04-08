e56ba28b01085f068c2f0376bb0a1aa34ffb2e75
Taynara <taynarasantossousa09@gmail.com>
2025-04-08 20:20:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var diaSemana = Number(prompt("Digite um dia de semana de 1 a 7"))
+
+switch(diaSemana){
+    case 1:
+        alert("Domingo")
+        break;
+    case 2:
+        alert("segunda")
+        break;
+    case 3:
+        alert("Terça")
+        break;
+    case 4:
+        alert("Quarta")
+        break;
+    case 5:
+        alert("Quinta")
+        break;
+    case 6:
+        alert("Sexta")
+        break;
+    case 7:
+        alert("Sabado")
+        break;
+}
